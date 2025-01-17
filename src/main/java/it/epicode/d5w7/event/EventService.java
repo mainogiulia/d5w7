2265cc7b9e745527353b6c89f98859e6b0b09924
mainogiulia <giuliamaino97@gmail.com>
2025-01-17 11:27:54 +0100

    adds exceptions and delete events

--- src/main/java/it/epicode/d5w7/event/EventService.java
+++ src/main/java/it/epicode/d5w7/event/EventService.java
@@ -2,6 +2,8 @@ package it.epicode.d5w7.event;
 
 import it.epicode.d5w7.auth.AppUser;
 import it.epicode.d5w7.auth.AppUserRepository;
+import it.epicode.d5w7.exceptions.EventNotFoundException;
+import it.epicode.d5w7.exceptions.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
@@ -14,7 +16,7 @@ public class EventService {
 
     public Event createEvent(EventRequest eventRequest) {
         AppUser eventPlanner = appUserRepository.findById(eventRequest.getEventPlannerId())
-                .orElseThrow(() -> new RuntimeException("Event Planner not found"));
+                .orElseThrow(() -> new UserNotFoundException("Event Planner not found"));
         Event event = new Event();
         BeanUtils.copyProperties(eventRequest, event);
         event.setEventPlanner(eventPlanner);
@@ -23,12 +25,17 @@ public class EventService {
 
     public Event updateEvent(Long id, EventRequest eventRequest) {
         Event event = eventRepo.findById(id)
-                .orElseThrow(() -> new RuntimeException("Event not found"));
+                .orElseThrow(() -> new EventNotFoundException("Event not found"));
         AppUser eventPlanner = appUserRepository.findById(eventRequest.getEventPlannerId())
-                .orElseThrow(() -> new RuntimeException("Event Planner not found"));
+                .orElseThrow(() -> new UserNotFoundException("Event Planner not found"));
         BeanUtils.copyProperties(eventRequest, event);
         event.setEventPlanner(eventPlanner);
         return eventRepo.save(event);
+    }
 
+    public void deleteEvent(Long id) {
+        Event event = eventRepo.findById(id)
+                .orElseThrow(() -> new EventNotFoundException("Event not found"));
+        eventRepo.delete(event);
     }
 }
